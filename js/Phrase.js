45c5bcaa0fd52c2abf096f113677e71b00ee0e46
Yolis <yolisisabel28@gmail.com>
2023-11-05 18:30:09 -0800

    phrase parameter w lowerCase function working

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,13 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+//blueprint to create phrase objs
+class Phrase{
+    constructor(phrase){
+       this.phrase = phrase.toLowerCase()
+    }
+}
+
+const samplePhrase = new Phrase( 'hello word');
+console.log(samplePhrase, 'phrase');
