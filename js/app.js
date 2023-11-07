5455a7bc40f9da00bd5f8753d411c709bfe22dc1
Yolis <yolisisabel28@gmail.com>
2023-11-07 12:16:40 -0800

    console log actual html instead of phrase obj

--- js/app.js
+++ js/app.js
@@ -6,12 +6,9 @@
 //outcome of phrase and game 
 const phrase = new Phrase("Fish are friends, not food");
 const game = new Game();
-console.log(phrase);
-// //calling the class game, 
-// //grabbing phrases property
-// //for each phrase that's in the array 
-// //print out 'phrase index = phrase: ""'
-// //ex. 'phrase 1 = phrase: "Hello world";
-// newgame.phrases.forEach((phrase, index) =>{
-//     console.log(`Phrase ${index} = phrase: ${phrase.phrase}`);
-// })
+console.log(phrase.phrase);
+
+//console.log(phrase); 
+//prints actual object {'fish are friends, not food'}
+//console.log(phrase.phrase);
+//prints out actual html (text) => fish are friends, not food
