97b7e75274be2c2b0613b3b4b77448206a480799
Yolis <yolisisabel28@gmail.com>
2023-11-06 16:09:41 -0800

    created addphrase method for card placeholders

--- js/Phrase.js
+++ js/Phrase.js
@@ -2,12 +2,63 @@
  * Project 4 - OOP Game App
  * Phrase.js */
 
+//const phrase = newPhrase();
 //blueprint to create phrase objs
 class Phrase{
     constructor(phrase){
        this.phrase = phrase.toLowerCase()
     }
+    addPhraseToDisplay = function(){
+        let newPhrase = "";
+        let letter = document.createElement('li');
+        this.phrase.split("").forEach(letter =>{
+            if(letter === " "){
+                newPhrase += " "
+            } else {
+                newPhrase += `<li>${letter}</li>`;
+            }
+        })   
+        console.log(newPhrase);
+    }
 }
 
 const samplePhrase = new Phrase( 'hello word');
+samplePhrase.addPhraseToDisplay();
 console.log(samplePhrase, 'phrase');
+
+
+
+//what represents letter placeholders?
+//each letter is present by one 'li' elem
+//when player guesses letter correctly, empty box is replaced
+//with matched letter 
+
+
+
+//adds letter placeholders to be displayed when the game starts
+//do i know when the game starts?
+//note: each letter is presented by an empty box = <li> </li>
+//where am i going to display the info:
+//let displayPhrase = document.querySelector('#phrase')
+//1. we have a button 'game start'
+    //let button = getElementById('btn_reset')
+//button.addEventListener('click', (e) =>{
+    //if(button){
+        //1. i want to grab the hidden cards
+//         <div id="phrase" class="section">
+//     <ul>
+//         <li class="hide letter h">h</li>
+//         <li class="hide letter o">o</li>
+//         <li class="hide letter w">w</li>
+//         <li class="space"> </li>
+//         <li class="hide letter a">a</li>
+//         <li class="hide letter r">r</li>
+//         <li class="hide letter e">e</li>
+//         <li class="space"> </li>
+//         <li class="hide letter y">y</li>
+//         <li class="hide letter o">o</li>
+//         <li class="hide letter u">u</li>
+//     </ul>
+// </div>
+    //}
+//})
