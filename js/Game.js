949ee831a186506283e71ee239e99162956fe8fa
Yolis <yolisisabel28@gmail.com>
2023-11-06 18:14:46 -0800

    renamed class from guess to game

--- js/Game.js
+++ js/Game.js
@@ -2,10 +2,13 @@
  * Project 4 - OOP Game App
  * Game.js */
 
-class Guess{
+class Game{
     constructor(){
         this.missed = 0,
         this.phrases = [],
         this.activePhrase = null 
     }
 }
+
+
+
