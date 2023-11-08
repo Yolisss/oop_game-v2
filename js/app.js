83a4c6fea090cfd49d6290359d71f8fbee2ec80a
Yolis <yolisisabel28@gmail.com>
2023-11-07 20:24:38 -0800

    button functionality to start game working

--- js/app.js
+++ js/app.js
@@ -2,41 +2,16 @@
  * Project 4 - OOP Game App
  * app.js */
 
-//creating instances of phrase and game 
-//outcome of phrase and game 
-//const phrase = new Phrase("Fish are friends, not food");
-//const game = new Game();
-const game = new Game();
-game.startGame();
-//console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
 
-// const randomPhrase = game.getRandomPhrase();
-// const phrase = new Phrase(randomPhrase);
-// phrase.addPhraseToDisplay();
+const button = document.querySelector("#btn__reset");
 
-game.phrases.forEach((phrase, index) =>{
-    //phrase reps object {phrase: ""}
-    //ASK FOR CLARIFICATION     
-    //game.phrases is giving us access to class game 
-    //then grabbing phrases property
-    //within the property for each PHRASE (orange), grab property phrase 
-    console.log(`Phrase ${index} - phrase: ${phrase.phrase}`)
+//blueprint but no description for game class 
+let game;
+console.log(button, 'button');
+
+button.addEventListener('click', (e)=>{
+//w new Game() as value, it's allowing us to access game class
+        game = new Game();
+        //accessing startGame within game class
+        game.startGame();
 })
-//ASK VLAD
-//removed the string "fish are friends, not food" but got error msg 
-//toLowerCase undefined.. why? 
-
-//phrase.addPhraseToDisplay();
-
-
-// console.log(game.getRandomPhrase());
-// console.log(game.getRandomPhrase());
-// console.log(game.getRandomPhrase());
-
-
-
-
-//console.log(phrase); 
-//prints actual object {'fish are friends, not food'}
-//console.log(phrase.phrase);
-//prints out actual html (text) => fish are friends, not food
